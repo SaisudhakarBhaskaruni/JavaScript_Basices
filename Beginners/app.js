1bc6cca3bead0db376632eb3e4eb517f17c378b5
saisudhakarBhaskaruni <1690315lnpvsaisudhakar@gmail.com>
2025-01-24 10:37:04 +0530

    new files for basices

---
+++ Beginners/app.js
@@ -0,0 +1,9 @@
+let name = "sai sudhakar";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
+let twitterHandle = "twitter handle";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
